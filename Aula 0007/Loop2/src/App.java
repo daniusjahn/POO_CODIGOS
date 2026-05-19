ed0ef8187212eab0dc48b3fae9c29c67ef6f4787
Carlos Daniel <140536944+daniusjahn@users.noreply.github.com>
2026-05-19 11:40:40 +0000

    Nessa Aula aprendemos a usar o while , for.

---
+++ Aula 0007/Loop2/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 1;
+        while (num <= 10 ){
+        System.out.println("Numero: " + num);
+        num = num + 1;
+        }
+        System.out.println("Fim!!");
+    }
+}
